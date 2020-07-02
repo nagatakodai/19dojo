551368464665f20f1f12b104debcfc070e229e9b
nagatakodai <53686206+nagatakodai@users.noreply.github.com>
2020-07-02 15:15:35 +0900

    各fragmentのonCreateViewを変更

--- app/src/main/java/com/nkjp/a19dojo_kotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nkjp/a19dojo_kotlin/ui/home/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.nkjp.a19dojo_kotlin.ui.home
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.nkjp.a19dojo_kotlin.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -21,4 +27,35 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE) ?: return
+        val name = sharedPref.getString(getString(R.string.name_key), "")
+        val github = sharedPref.getString(getString(R.string.github_key), "")
+        val twitter = sharedPref.getString(getString(R.string.twitter_key), "")
+
+        edit_name.setText(name)
+        edit_github.setText(github)
+        edit_twitter.setText(twitter)
+
+        saveButton.setOnClickListener {
+            this.onSave(
+                requireActivity(),
+                edit_name.text,
+                edit_github.text,
+                edit_twitter.text
+            )
+        }
+    }
+    //ViewModel
+    private fun onSave(activity :Activity,edit_name :Editable?,edit_github :Editable?,edit_twitter :Editable?){
+        val name = edit_name.toString()
+        val github = edit_github.toString()
+        val twitter = edit_twitter.toString()
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+        sharedPref.edit {
+            putString(activity.getString(R.string.name_key),name)
+            putString(activity.getString(R.string.github_key),github)
+            putString(activity.getString(R.string.twitter_key),twitter)
+        }
+    }
 }
