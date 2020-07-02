e9b22c1cd8c67de26ec50d8a0b614f171428931c
nagatakodai <53686206+nagatakodai@users.noreply.github.com>
2020-07-02 19:44:09 +0900

    QR読み取り

--- app/src/main/java/com/nkjp/a19dojo_kotlin/ui/qr/QrFragment.kt
+++ app/src/main/java/com/nkjp/a19dojo_kotlin/ui/qr/QrFragment.kt
@@ -1,24 +1,23 @@
 package com.nkjp.a19dojo_kotlin.ui.qr
 
+import android.R.attr
 import android.content.Context
-import android.media.Image
+import android.content.Intent
 import android.os.Bundle
 import android.util.AndroidRuntimeException
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
+import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.nkjp.a19dojo_kotlin.R
-import com.nkjp.a19dojo_kotlin.ui.home.HomeFragment
 import kotlinx.android.synthetic.main.fragment_qr.*
+
 
 class QrFragment : Fragment() {
 
@@ -37,6 +36,7 @@ class QrFragment : Fragment() {
         val github = sharedPref.getString(getString(R.string.github_key), "")
         val twitter = sharedPref.getString(getString(R.string.twitter_key), "")
         val qrData = "ca-tech://dojo/share?iam=$name&tw=$twitter&gh=$github"
+        Toast.makeText(activity,"読み込みました",Toast.LENGTH_SHORT).show()
         try {
             val barcodeEncoder = BarcodeEncoder()
             val bitmap = barcodeEncoder.encodeBitmap(qrData, BarcodeFormat.QR_CODE, 500, 500)
@@ -44,6 +44,19 @@ class QrFragment : Fragment() {
             qrText.text = "あなたのURLは、$qrData です。"
         }catch (e: WriterException){
             throw AndroidRuntimeException("Barcode Error.",e)
+        }
+        qrReadButton.setOnClickListener{
+            IntentIntegrator(this.activity).initiateScan()
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            Log.d("readQR", result.contents)
+            Toast.makeText(activity, result.contents,Toast.LENGTH_LONG).show()
+            Toast.makeText(activity,"読み込みました",Toast.LENGTH_SHORT).show()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
